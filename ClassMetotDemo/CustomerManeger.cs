7860505ce425ed37f4ad37d85e87d7c4b32887bd
gozdetoz <gozdetoz62@gmail.com>
2021-01-18 15:22:28 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManeger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManeger
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added " + customer.Name+" " +customer.Surname );
+        }
+
+        public void Get(Customer customer)
+        {
+           
+            Console.WriteLine("List :" + customer.Name + " " + customer.Surname);
+        }
+
+        public void Delete(Customer customer)
+        {
+             Console.WriteLine("Customer is deleted "  + customer.Name + " " + customer.Surname);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Gözde";
+            customer1.Surname = "Toz";
+            Customer customer2 = new Customer();
+            customer2.Name = "Tuncay";
+            customer2.Surname = "Can";
+            Customer customer3 = new Customer();
+            customer3.Name = "Burak";
+            customer3.Surname = "Toz";
+
+            CustomerManeger customerManager = new CustomerManeger();
+
+            customerManager.Add(customer1);
+            customerManager.Get(customer2);
+            customerManager.Delete(customer3);
+            
+        }
+    }
+}
